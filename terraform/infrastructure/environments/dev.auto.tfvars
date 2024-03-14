00f86897ace1736ef509331a98b574167a24c7cb
root <root@ip-172-31-13-227.ap-southeast-2.compute.internal>
2024-03-14 05:45:44 +0000

    Add first code

---
+++ terraform/infrastructure/environments/dev.auto.tfvars
@@ -0,0 +1,3 @@
+project_id = "nprd-terraform-training"
+account_id = "terraform-test-account"
+display_name = "Service Account"
---
+++ terraform/infrastructure/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+  credentials = "/root/google.json"
+}
