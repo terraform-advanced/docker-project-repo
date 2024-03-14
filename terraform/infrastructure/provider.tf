9dd16b6448a5ea964d23ae36cb22362a07a07353
root <root@ip-172-31-13-227.ap-southeast-2.compute.internal>
2024-03-14 07:44:15 +0000

    Add new final one

--- terraform/infrastructure/provider.tf
+++ terraform/infrastructure/provider.tf
@@ -2,5 +2,5 @@ provider "google" {
   project = "nprd-terraform-training"
   region  = "us-central1"
   zone    = "us-central1-c"
-  credentials = file("/home/mohit/storage.json")
+  credentials = file("/root/docker-project-repo/terraform/infrastructure/storage.json")
 }
