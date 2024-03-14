0b0bb0d0e06b2aaea6d73a1c702a9b771648143b
root <root@ip-172-31-13-227.ap-southeast-2.compute.internal>
2024-03-14 08:48:11 +0000

    Add new sa

--- terraform/infrastructure/environments/dev.auto.tfvars
+++ terraform/infrastructure/environments/dev.auto.tfvars
@@ -1,5 +1,4 @@
 
 #####dev auto tf var file####
 project_id = "nprd-terraform-training"
-account_id = "terraform-test-account"
-display_name = "Service Account"
+account_id = "terraform-updated-sa-account"
---
+++ terraform/infrastructure/sa.tf
@@ -0,0 +1,12 @@
+
+resource "google_service_account" "sa" {
+  account_id   = var.account_id
+  display_name = "A service account that Jane can use"
+}
+
+resource "google_service_account_iam_member" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.sa.email}"
+}
+
